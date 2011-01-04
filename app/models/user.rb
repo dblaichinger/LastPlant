fbf057e54bba486aab6bbe616a897898fbbaeef1
Daniel Blaichinger <dblaichinger.mmt-b2009@fh-salzburg.ac.at>
2011-01-04 12:46:18 +0100

    Initial commit for combining projects

---
+++ app/models/user.rb
@@ -0,0 +1,90 @@
+# == Schema Information
+# Schema version: 20101230150822
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+	
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
+	
+	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+
+	
+	validates :name, :presence => true, :length => {:maximum =>50}
+
+	validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+
+
+	  # Automatically create the virtual attribute 'password_confirmation'.
+	  validates :password, :presence     => true,
+						   :confirmation => true,
+						   :length       => { :within => 6..40 }
+
+	 
+	 #Calls method encrypt_password before saving to database
+	 before_save :encrypt_password
+	 
+	 # Return true if the user's password matches the submitted password.
+  	 def has_password?(submitted_password)
+     # Compare encrypted_password with the encrypted version of submitted_password.
+	 	encrypted_password == encrypt(submitted_password)
+  	 end
+
+	  def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		if user.nil?
+		  return nil
+		elsif user.has_password?(submitted_password)
+		  return user
+		else
+		  return nil
+		end
+	  end
+
+
+	
+	  def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil  if user.nil?
+		return user if user.has_password?(submitted_password)
+	  end
+
+
+	def self.authenticate_with_salt(id, cookie_salt)
+	  user = find_by_id(id)
+	  return nil  if user.nil?
+	  return user if user.salt == cookie_salt
+	end
+
+
+	  private
+	
+		def encrypt_password
+		  self.salt = make_salt if new_record?
+		  self.encrypted_password = encrypt(password)
+		end
+	
+		def encrypt(string)
+		  secure_hash("#{salt}--#{string}")
+		end
+	
+		def make_salt
+		  secure_hash("#{Time.now.utc}--#{password}")
+		end
+	
+		def secure_hash(string)
+		  Digest::SHA2.hexdigest(string)
+		end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '838820f8b2315f4f11988a9b9c9209ac3fb97c48baa1f3c562cbbdd1582f44af7c3597d65a05edc436a0a8619a9fe20a1baec475319d513f414a6e694f56b817'
