a873dc15ef69f7a78a9c67ded4f1e0c0a3e9f114
Daniel Blaichinger <dblaichinger.mmt-b2009@fh-salzburg.ac.at>
2011-01-05 18:26:01 +0100

    Commit for andre

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@
 class User < ActiveRecord::Base
 	
 	attr_accessor :password
-	attr_accessible :name, :email, :password#, :password_confirmation
+	attr_accessible :name, :email, :password, :password_confirmation, :isFacebook
 	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
