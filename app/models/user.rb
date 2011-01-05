6f883ad999a9cb41d6e294b331fd7cdf8feaf7a5
Daniel Blaichinger <dblaichinger.mmt-b2009@fh-salzburg.ac.at>
2011-01-05 18:16:44 +0100

    Commit for andre

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,11 +33,11 @@ class User < ActiveRecord::Base
 
 
 	  # Automatically create the virtual attribute 'password_confirmation'.
-	
-	  validates  :password,# :presence     => true,
-	 					   #:confirmation => true,
+	=begin
+	  validates  :password, :presence     => true,
+	 					   :confirmation => true,
 						   :length       => { :within => 6..40 }
-	
+	=end
 	 
 						  
 
