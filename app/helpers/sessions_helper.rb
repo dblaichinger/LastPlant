3bd5164bde7acf6c826dcd57296b66e071a4407c
Andre Schweighofer <nopunktfjutscha@gmail.com>
2011-01-06 12:30:54 +0100

    fu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,11 +11,11 @@ def sign_in(user)
   end
 
   def current_user
-    @current_user ||= user_from_remember_token 
+    @current_user ||= user_from_remember_token ||= User.find_by_fbid(session[:fb_id])
   end
 
   def signed_in?
-    !current_user.nil? || session[:fb_id]
+    !current_user.nil?
   end
 
   def sign_out
