9bc8fbdf4b819c65640c544a72a4cf6181e4f574
Andre Schweighofer <nopunktfjutscha@gmail.com>
2011-01-06 11:24:25 +0100

    session helper updated

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,7 @@ def sign_in(user)
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+    @current_user ||= user_from_remember_token ||= User.find_by_fbid(session[:fb_id])
   end
 
   def signed_in?
