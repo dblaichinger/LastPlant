e0b262a6b4213de18b4086e01ed784ef6c960ffb
Daniel Blaichinger <dblaichinger.mmt-b2009@fh-salzburg.ac.at>
2011-01-08 18:03:40 +0100

    Change content type

--- db/migrate/20110106163234_create_maps.rb
+++ db/migrate/20110106163234_create_maps.rb
@@ -2,7 +2,6 @@ class CreateMaps < ActiveRecord::Migration
   def self.up
     create_table :maps do |t|
       t.string :name
-      t.text :content
       t.integer :score
       t.integer :user_id
       
