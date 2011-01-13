a082be63b9bade4c32ec671bc41c42727f4438ce
Daniel Blaichinger <dblaichinger.mmt-b2009@fh-salzburg.ac.at>
2011-01-13 13:06:33 +0100

    merge error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
   def index
     @title = "All users"
-    @users = User.all
+ 
+	@users = User.paginate(:page => params[:page])
   end
   
   def show  
