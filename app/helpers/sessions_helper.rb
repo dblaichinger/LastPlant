907546ed14a9071c5e688a4c33efeb6c9500512d
Andre Schweighofer <nopunktfjutscha@gmail.com>
2011-01-21 16:42:20 +0100

    fb new user old

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -101,14 +101,8 @@ module SessionsHelper
                 
         #else create new user
 			else
-                params[:user][:fbid] = @me['id']
-                params[:user][:name] = @me['name']
-                params[:user][:email] = @me['email']
-                params[:user][:password] = @me['id']
-                params[:user][:password_confirmation] = @me['id']
-                
-				@user = User.register_new(params[:user], true)
-                
+               @user = User.new(:fbid => @me['id'], :name => @me['name'], :email => @me['email'], :isFacebook => true, :password =>@me['id'], :password_confirmation =>@me['id'], :createScore => 0, :destroyScore => 0)
+                 
         #if creating user worked, set session
 				if @user.save
           session[:fb_id] = @user.fbid
