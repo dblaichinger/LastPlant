e5ff588053beddcb65f72272ca41b9eff96e260f
Daniel Blaichinger <daniel.blaichi@gmx.at>
2011-03-07 14:41:18 +0100

    fb login fix

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -103,13 +103,14 @@ module SessionsHelper
                 
         #else create new user
 			else
-                params[:user][:fbid] = @me['id']
-                params[:user][:name] = @me['name']
-                params[:user][:email] = @me['email']
-                params[:user][:password] = @me['id']
-                params[:user][:password_confirmation] = @me['id']
+			@user = User.new(:fbid => @me['id'], :name => @me['name'], :email => @me['email'], :isFacebook => true, :password =>@me['id'], :password_confirmation =>@me['id'], :createScore => 0, :destroyScore => 0)
+            #    params[:user][:fbid] = @me['id']
+             #   params[:user][:name] = @me['name']
+              #  params[:user][:email] = @me['email']
+               # params[:user][:password] = @me['id']
+                #params[:user][:password_confirmation] = @me['id']
                 
-				@user = User.register_new(params[:user], true)
+				#@user = User.register_new(params[:user], true)
                 
         #if creating user worked, set session
 				if @user.save
